808dd2031312b0b94bdb1ab37cf5499e029265fc
Stephen Oba <obastepheno@gmail.com>
2022-10-11 04:58:49 +0100

    final implementation of insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,75 +1,43 @@
 #include "sort.h"
 
-void insert(listint_t **list, listint_t *current, listint_t *new_node)
+/**
+ * rswap - recursively swap nodes
+ * @list: Doubly linked list
+ * @current: Current
+ * @new_node: node to be swapped
+ */
+void rswap(listint_t **list, listint_t *current, listint_t *new_node)
 {
 	listint_t *previous;
 
-	if (current->prev != NULL && current->n > new_node->n)
-	{
-		previous = current->prev;
-		previous->next = new_node;
-		current->prev = new_node;
-		if (new_node->next != NULL)
-			current->next = new_node->next;
-		new_node->next = current;
-		new_node->prev = previous;
-		print_list(*list);
-		insert(list, previous, new_node);
-	}
+	if (current->n <= new_node->n)
+		return;
+
 	else if (current->prev == NULL)
 	{
-		new_node->next = current;
+		new_node->prev = current->prev;
+		if (new_node->next != NULL)
+			new_node->next->prev = current;
 		current->prev = new_node;
+		current->next = new_node->next;
+		new_node->next = current;
 		*list = new_node;
 		print_list(*list);
 	}
-	/*
-	if ((*list)->n > new_node->n)
-	{
-		new_node->next = *list;
-		new_node->next->prev = new_node;
-		*list = new_node;
-	}
 	else
 	{
-		while (current->next != NULL && current->next->n < new_node->n)
-		{
-			current = current->next;
-			new_node->next = current->next;
-			if (current->next !=NULL)
-				new_node->next->prev = new_node;
-			current->next = new_node;
-			new_node->prev = current;
-		}
-	}
-	*/
-}
-/*
-
-	if (*list == NULL)
-		*list = new_node;
-	else if ((*list)->n >= new_node->n)
-	{
-		new_node->next = *list;
-		new_node->next->prev = new_node;
-		*list = new_node;
-	}
-	else
-	{
-		current = *list;
-
-		while (current->next != NULL && current->next->n < new_node->n)
-			current = current->next;
-
-		new_node->next = current->next;
-
-		if (current->next != NULL)
-			new_node->next->prev = new_node;
-		current->next = new_node;
-		new_node->prev = current;
+		previous = current->prev;
+		current->prev->next = new_node;
+		new_node->prev = current->prev;
+		if (new_node->next != NULL)
+			new_node->next->prev = current;
+		current->prev = new_node;
+		current->next = new_node->next;
+		new_node->next = current;
+		print_list(*list);
+		rswap(list, previous, previous->next);
 	}
 }
-*/
 
 /**
  * insertion_sort_list - Implementation of the insertion sort algorithm
@@ -82,17 +50,12 @@ void insertion_sort_list(listint_t **list)
 	while (current != NULL && current->next != NULL)
 	{
 		next = current->next;
-		printf("%d-- cur %d\n", next->n, current->n);
-		if (next->n < current->n)
+		if (current->n > next->n)
 		{
-			current->next = next->next;
-			if (next->next)
-				next->next->prev = current;
-			next->next = next->prev =  NULL;
-			insert(list, current, next);
+			current->next = current->next->next;
 		}
 		else
 			current = current->next;
-		print_list(*list);
+		rswap(list, current, next);
 	}
 }
--- 1-main.c
+++ 1-main.c
@@ -41,7 +41,7 @@ listint_t *create_listint(const int *array, size_t size)
 int main(void)
 {
     listint_t *list;
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    int array[] = {29, 85, 93, 44, 52, 37, 67, 74, 32, 26};
     size_t n = sizeof(array) / sizeof(array[0]);
 
     list = create_listint(array, n);
