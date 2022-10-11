b584cf4e2064cc584770ad7808af8644ef763c85
Stephen Oba <obastepheno@gmail.com>
2022-10-11 06:03:51 +0100

    Implement selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - implementation of selection sort algorithm
+ * @array: Array to be sorted
+ * @size: Size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+				continue;
+			}
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
