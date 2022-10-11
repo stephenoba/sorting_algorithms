21a819967c0564fe0cf7e81fe93478c2032d8a93
Stephen Oba <obastepheno@gmail.com>
2022-10-11 07:34:24 +0100

    first implementation of quick sort algorithm

---
+++ 3-quick_sort.c
@@ -0,0 +1,54 @@
+ #include "sort.h"
+
+/**
+ * swap - swap elements in array
+ * @array: Array
+ * @i: first element
+ * @j: second element
+ */
+void swap(int *array, int i, int j, size_t size)
+{
+	int t = array[i];
+	
+	array[i] = array[j];
+	array[j] = t;
+	print_array(array, size);
+}
+
+void sort(int *array, size_t low, size_t high, size_t size)
+{
+	size_t i, j, pivot; 
+
+	if(low < high)
+	{
+		pivot = low;
+		i = low;
+		j = high;
+
+		while(i < j)
+		{
+			while(array[i] <= array[pivot] && i < high)
+				i++;
+			while(array[j] > array[pivot])
+				j--;
+			if(i < j)
+			{
+				swap(array, i, j, size);
+			}
+		}
+		swap(array, pivot, j, size);
+		sort(array, low, j - 1, size);
+		sort(array, j + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - implementation of quick sort algorithm
+ * @array: Array to sort
+ * @size: size of array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array && size > 1)
+		sort(array, 0, size - 1, size);
+}
